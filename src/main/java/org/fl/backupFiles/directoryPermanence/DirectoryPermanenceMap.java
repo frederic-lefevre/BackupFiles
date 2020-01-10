36ea375b9efc683dabc8c3d03b5cb8071a23f7df
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2020-01-10 14:18:51 +0100

    Implement permancence map

--- src/main/java/org/fl/backupFiles/directoryPermanence/DirectoryPermanenceMap.java
+++ src/main/java/org/fl/backupFiles/directoryPermanence/DirectoryPermanenceMap.java
@@ -1,17 +1,23 @@
 package org.fl.backupFiles.directoryPermanence;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 
 public class DirectoryPermanenceMap implements DirectoryPermanence {
 
+	private static final String PATH 	   = "path" ;
+	private static final String PERMANENCE = "permanence" ;
+	
 	private final TreeMap<Path,DirectoryPermanenceLevel> permanenceMap ;
 	private final Set<Path> pathKeys ;
 	
@@ -22,7 +28,14 @@ public class DirectoryPermanenceMap implements DirectoryPermanence {
 		if (jsonConfig != null) {
 			
 			try {
-				JsonElement jElemConf = new JsonParser().parse(jsonConfig) ;
+				JsonArray jPaths = new JsonParser().parse(jsonConfig).getAsJsonArray() ;
+				for (JsonElement jElem : jPaths) {
+					JsonObject jPathPermanence = jElem.getAsJsonObject() ;
+					
+					Path sPath = Paths.get(jPathPermanence.get(PATH).getAsString()) ;
+					DirectoryPermanenceLevel sPermanence = DirectoryPermanenceLevel.valueOf(jPathPermanence.get(PERMANENCE).getAsString()) ;
+					permanenceMap.put(sPath, sPermanence) ;
+				}
 		
 			} catch (JsonSyntaxException e) {
 				bLog.log(Level.SEVERE, "Invalid JSON configuration: " + jsonConfig, e) ;
@@ -37,11 +50,16 @@ public class DirectoryPermanenceMap implements DirectoryPermanence {
 	public DirectoryPermanenceLevel getPermanenceLevel(Path dir) {
 
 		for (Path pathKey : pathKeys) {
-			if (dir.startsWith(pathKey)) {
+			if (intersectWith(dir, pathKey)) {
 				return permanenceMap.get(pathKey) ;
 			}
 		}
 		return DEFAULT_PERMANENCE_LEVEL;
 	}
 
+	// nativePath maybe a windows path. 
+	// stdPath has "/" as separator
+	private boolean intersectWith(Path nativePath, Path stdPath) {
+		return nativePath.toString().contains(stdPath.toString()) ;
+	}
 }
--- src/main/java/org/fl/backupFiles/directoryPermanence/PermanencePathComparator.java
+++ src/main/java/org/fl/backupFiles/directoryPermanence/PermanencePathComparator.java
@@ -8,7 +8,9 @@ public class PermanencePathComparator implements Comparator<Path> {
 	@Override
 	public int compare(Path path1, Path path2) {
 		
-		if (path1.startsWith(path2)) {
+		if (path1.equals(path2)) {
+			return 0 ;
+		} else if (path1.startsWith(path2)) {
 			return -1 ;
 		} else if (path2.startsWith(path1)) {
 			return 1 ;
