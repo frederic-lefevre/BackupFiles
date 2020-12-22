09b41561c5439ad216eec5f6fafe5fdfcdee5b74
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2020-12-22 09:36:37 +0100

    Fix warnings

--- src/test/java/org/fl/backupFiles/BackUpJobTest.java
+++ src/test/java/org/fl/backupFiles/BackUpJobTest.java
@@ -3,12 +3,10 @@ package org.fl.backupFiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.net.URI;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
