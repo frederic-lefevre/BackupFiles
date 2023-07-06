619f73366dd20869903ebebb8e40562a8c781670
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-07-06 10:07:20 +0200

    Allow single row selection only

--- src/main/java/org/fl/backupFiles/gui/BackUpJTable.java
+++ src/main/java/org/fl/backupFiles/gui/BackUpJTable.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2023 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.fl.backupFiles.gui;
 
 import java.util.ArrayList;
@@ -5,6 +29,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import org.fl.backupFiles.BackUpItem ;
@@ -16,14 +41,19 @@ public class BackUpJTable extends JTable {
 	private Logger tLog ;
 
 	public BackUpJTable(TableModel arg0, Logger l) {
-		super(arg0);
-		
-		init(l) ;	
+		super(arg0);	
+		init(l);	
 	}
 
 	public BackUpJTable(TableModel arg0, TableColumnModel arg1, Logger l) {
 		super(arg0, arg1);
-		init(l) ;
+		init(l);
+	}
+
+	
+	public BackUpJTable(TableModel dm, TableColumnModel cm, ListSelectionModel sm, Logger l) {
+		super(dm, cm, sm);
+		init(l);
 	}
 
 	private void init(Logger l) {
@@ -40,6 +70,7 @@ public class BackUpJTable extends JTable {
 		getColumnModel().getColumn(BackUpTableModel.ACTION_COL_IDX).setPreferredWidth(120);
 		getColumnModel().getColumn(BackUpTableModel.STATUS_COL_IDX).setPreferredWidth(100);
 		getColumnModel().getColumn(BackUpTableModel.TARGET_PATH_COL_IDX).setPreferredWidth(745);
+
 		setAutoResizeMode(JTable.AUTO_RESIZE_OFF) ;
 		
 		addMouseListener(new BackupItemMouseAdapter(this, Config.getOsActions(), tLog));
