7aba7316730a252993c878a94ee6dbd9aa495124
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-23 16:16:08 +0200

    Add new counter test

--- src/test/java/org/fl/backupFiles/BackUpCountersTest.java
+++ src/test/java/org/fl/backupFiles/BackUpCountersTest.java
@@ -32,55 +32,55 @@ class BackUpCountersTest {
 
 	@Test
 	void shouldBeZeroAtCreation() {
-		
-		BackUpCounters bc = new BackUpCounters() ;	
-		assertFieldValue(bc, 0) ;
+
+		BackUpCounters bc = new BackUpCounters();
+		assertFieldValue(bc, 0);
 	}
-	
-	@Test	
+
+	@Test
 	void shouldBeZeroAtReset() {
-		
-		BackUpCounters bc = new BackUpCounters() ;
+
+		BackUpCounters bc = new BackUpCounters();
 		bc.reset();
-		assertFieldValue(bc, 0) ;
+		assertFieldValue(bc, 0);
 		setFieldValue(bc, 12);
-		assertFieldValue(bc, 12) ;
+		assertFieldValue(bc, 12);
 		bc.reset();
-		assertFieldValue(bc, 0) ;
+		assertFieldValue(bc, 0);
 	}
 	
-	@Test	
+	@Test
 	void shouldAddCounters() {
-		
-		BackUpCounters bc1 = new BackUpCounters() ;
-		BackUpCounters bc2 = new BackUpCounters() ;
+
+		BackUpCounters bc1 = new BackUpCounters();
+		BackUpCounters bc2 = new BackUpCounters();
 
 		setFieldValue(bc1, 12);
-		assertFieldValue(bc1, 12) ;
-		
+		assertFieldValue(bc1, 12);
+
 		setFieldValue(bc2, 11);
-		assertFieldValue(bc2, 11) ;
-		
+		assertFieldValue(bc2, 11);
+
 		bc1.add(bc2);
-		assertFieldValue(bc1, 23) ;
-		assertFieldValue(bc2, 11) ;
+		assertFieldValue(bc1, 23);
+		assertFieldValue(bc2, 11);
 	}
-	
-	@Test	
+
+	@Test
 	void shouldAddCountersWithIncrement() {
-		
-		BackUpCounters bc1 = new BackUpCounters() ;
-		BackUpCounters bc2 = new BackUpCounters() ;
+
+		BackUpCounters bc1 = new BackUpCounters();
+		BackUpCounters bc2 = new BackUpCounters();
 
 		setFieldValueWithIncrement(bc1, 12);
-		assertFieldValueWithIncrement(bc1, 12, 1) ;
-		
+		assertFieldValueWithIncrement(bc1, 12, 1);
+
 		setFieldValueWithIncrement(bc2, 11);
-		assertFieldValueWithIncrement(bc2, 11, 1) ;
-		
+		assertFieldValueWithIncrement(bc2, 11, 1);
+
 		bc1.add(bc2);
-		assertFieldValueWithIncrement(bc1, 23, 2) ;
-		assertFieldValueWithIncrement(bc2, 11, 1) ;
+		assertFieldValueWithIncrement(bc1, 23, 2);
+		assertFieldValueWithIncrement(bc2, 11, 1);
 	}
 	
 	private static void assertFieldValue(BackUpCounters bc, long val) {
@@ -100,6 +100,7 @@ class BackUpCountersTest {
 		assertThat(bc.nbTargetFilesFailed).isEqualTo(val);
 		assertThat(bc.nbTargetFilesProcessed).isEqualTo(val);
 		assertThat(bc.totalSizeDifference).isEqualTo(val);
+		assertThat(bc.copyTargetNb).isEqualTo(val);
 	}
 
 	private static void assertFieldValueWithIncrement(BackUpCounters bc, long val, long m) {
@@ -119,43 +120,46 @@ class BackUpCountersTest {
 		assertThat(bc.nbTargetFilesFailed).isEqualTo(val+12*m);
 		assertThat(bc.nbTargetFilesProcessed).isEqualTo(val+13*m);
 		assertThat(bc.totalSizeDifference).isEqualTo(val+14*m);
+		assertThat(bc.copyTargetNb).isEqualTo(val+15*m);
 	}
 	
 	private static void setFieldValue(BackUpCounters bc, long val) {
-		
-		bc.ambiguousNb = val ;
-		bc.backupWithSizeAboveThreshold = val ;
-		bc.contentDifferentNb = val ;
-		bc.copyNewNb = val ;
-		bc.copyReplaceNb = val ;
-		bc.copyTreeNb = val ;
-		bc.deleteDirNb = val ;
-		bc.deleteNb = val ;
-		bc.nbHighPermanencePath = val ;
-		bc.nbMediumPermanencePath = val ;
-		bc.nbSourceFilesFailed = val ;
-		bc.nbSourceFilesProcessed = val ;
-		bc.nbTargetFilesFailed = val ;
-		bc.nbTargetFilesProcessed = val ;
-		bc.totalSizeDifference = val ;
+
+		bc.ambiguousNb = val;
+		bc.backupWithSizeAboveThreshold = val;
+		bc.contentDifferentNb = val;
+		bc.copyNewNb = val;
+		bc.copyReplaceNb = val;
+		bc.copyTreeNb = val;
+		bc.deleteDirNb = val;
+		bc.deleteNb = val;
+		bc.nbHighPermanencePath = val;
+		bc.nbMediumPermanencePath = val;
+		bc.nbSourceFilesFailed = val;
+		bc.nbSourceFilesProcessed = val;
+		bc.nbTargetFilesFailed = val;
+		bc.nbTargetFilesProcessed = val;
+		bc.totalSizeDifference = val;
+		bc.copyTargetNb = val;
 	}
-	
+
 	private static void setFieldValueWithIncrement(BackUpCounters bc, long val) {
-		
-		bc.ambiguousNb = val ;
-		bc.backupWithSizeAboveThreshold = val+1 ;
-		bc.contentDifferentNb = val+2 ;
-		bc.copyNewNb = val+3 ;
-		bc.copyReplaceNb = val+4 ;
-		bc.copyTreeNb = val+5 ;
-		bc.deleteDirNb = val+6 ;
-		bc.deleteNb = val+7 ;
-		bc.nbHighPermanencePath = val+8 ;
-		bc.nbMediumPermanencePath = val+9 ;
-		bc.nbSourceFilesFailed = val+10 ;
-		bc.nbSourceFilesProcessed = val+11 ;
-		bc.nbTargetFilesFailed = val+12 ;
-		bc.nbTargetFilesProcessed = val+13 ;
-		bc.totalSizeDifference = val+14 ;
+
+		bc.ambiguousNb = val;
+		bc.backupWithSizeAboveThreshold = val + 1;
+		bc.contentDifferentNb = val + 2;
+		bc.copyNewNb = val + 3;
+		bc.copyReplaceNb = val + 4;
+		bc.copyTreeNb = val + 5;
+		bc.deleteDirNb = val + 6;
+		bc.deleteNb = val + 7;
+		bc.nbHighPermanencePath = val + 8;
+		bc.nbMediumPermanencePath = val + 9;
+		bc.nbSourceFilesFailed = val + 10;
+		bc.nbSourceFilesProcessed = val + 11;
+		bc.nbTargetFilesFailed = val + 12;
+		bc.nbTargetFilesProcessed = val + 13;
+		bc.totalSizeDifference = val + 14;
+		bc.copyTargetNb = val + 15;
 	}
 }
