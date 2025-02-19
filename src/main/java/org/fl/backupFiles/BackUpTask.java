e3c78f0f20b1e5e807b59d4ce6c5db0d0fa08f81
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-02-19 21:58:16 +0100

    Change logger

--- src/main/java/org/fl/backupFiles/BackUpTask.java
+++ src/main/java/org/fl/backupFiles/BackUpTask.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2023 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,7 @@ import java.util.logging.Logger;
 
 public class BackUpTask {
 
-	private static final Logger bLog = Config.getLogger();
+	private static final Logger bLog = Logger.getLogger(BackUpTask.class.getName());
 	
 	private final Path source;
 	private final Path target;
