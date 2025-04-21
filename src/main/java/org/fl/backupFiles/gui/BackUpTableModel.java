1eade5a34591123e9cf6bb61847cfa43cf04d176
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-04-21 09:29:16 +0200

    Refactor

--- src/main/java/org/fl/backupFiles/gui/BackUpTableModel.java
+++ src/main/java/org/fl/backupFiles/gui/BackUpTableModel.java
@@ -89,25 +89,18 @@ public class BackUpTableModel extends AbstractTableModel {
 	 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		switch(columnIndex){
-        case SOURCE_PATH_COL_IDX:
-            return backUpItems.get(rowIndex).getSourcePath();
-        case SIZE_DIFF_COL_IDX:
-        	return backUpItems.get(rowIndex);
-        case PERMANENCE_COL_IDX:
-        	return backUpItems.get(rowIndex).getPermanenceLevel();
-        case ACTION_COL_IDX:
-            return backUpItems.get(rowIndex).getBackupAction();
-        case STATUS_COL_IDX:
-            return backUpItems.get(rowIndex).getBackupStatus();
-        case TARGET_PATH_COL_IDX:
-            return backUpItems.get(rowIndex).getTargetPath();
-        default:
-            return null;
-		}
+		return switch(columnIndex){
+        	case SOURCE_PATH_COL_IDX -> backUpItems.get(rowIndex).getSourcePath();
+        	case SIZE_DIFF_COL_IDX -> backUpItems.get(rowIndex);
+        	case PERMANENCE_COL_IDX -> backUpItems.get(rowIndex).getPermanenceLevel();
+        	case ACTION_COL_IDX -> backUpItems.get(rowIndex).getBackupAction();
+        	case STATUS_COL_IDX -> backUpItems.get(rowIndex).getBackupStatus();
+        	case TARGET_PATH_COL_IDX -> backUpItems.get(rowIndex).getTargetPath();
+        	default -> null;
+		};
 	}
 
 	public BackUpItem getBackUpItemAt(int rowIndex) {
-		return backUpItems.get(rowIndex) ;
+		return backUpItems.get(rowIndex);
 	}
 }
