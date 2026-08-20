8be2aca53571aea241c9540472990b6ff5e68a84
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-08-20 21:45:45 +0200

    Simplify with record

--- src/main/java/org/fl/backupFiles/OsAction.java
+++ src/main/java/org/fl/backupFiles/OsAction.java
@@ -24,27 +24,5 @@ SOFTWARE.
 
 package org.fl.backupFiles;
 
-public class OsAction {
-
-	private final String actionTitle;
-	private final String actionCommand;
-	private final boolean separateParam;
-
-	public OsAction(String t, String c, boolean s) {
-		actionTitle = t;
-		actionCommand = c;
-		separateParam = s;
-	}
-
-	public String getActionTitle() {
-		return actionTitle;
-	}
-
-	public String getActionCommand() {
-		return actionCommand;
-	}
-
-	public boolean paramSeparated() {
-		return separateParam;
-	}
+public record OsAction(String actionTitle, String actionCommand, boolean separateParam) {
 }
